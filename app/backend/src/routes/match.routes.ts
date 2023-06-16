fed5fdef2d8d9288a88d9658ff796d818d053e2e
Lennon <lennon_lxs@hotmail.com>
2023-06-16 14:14:24 -0300

    arruma a rota matches

--- app/backend/src/routes/match.routes.ts
+++ app/backend/src/routes/match.routes.ts
@@ -8,9 +8,5 @@ matchRouter.get(
   '/matches',
   (req: Request, res: Response) => matchController.getAllMatchs(req, res),
 );
-matchRouter.get(
-  '/matches/:id',
-  (req: Request, res: Response) => matchController.getMatchById(req, res),
-);
 
 export default matchRouter;
